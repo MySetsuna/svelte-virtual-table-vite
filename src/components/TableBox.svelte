7107bc38dedcc164391a05f23b7bed8865a6541b
D:/Program Files/DevTools/Git/jxk <1286783182@qq.com>
2022-11-21 01:21:23 +0800

    添加双向绑定

--- src/components/TableBox.svelte
+++ src/components/TableBox.svelte
@@ -2,18 +2,58 @@
   import VirtualList from "../lib/VirtualList";
   import { onMount } from "svelte";
 
+  /**
+   * @type {HTMLElement}
+   */
   let virtuallTableDom;
+  let virtuallTable;
+  let scrollTop = 0;
+  let scrollLeft = 0;
+  let scrollHeight = 0;
+  let scrolling = false;
+
+  /**
+   *
+   * @param e
+   */
+  const handleScroll = (e) => {
+    scrollTop = e.target.scrollTop;
+    scrollLeft = e.target.scrollLeft;
+    if (!scrolling) {
+      window.requestAnimationFrame(() => {
+        virtuallTable.scroll(scrollTop, scrollLeft);
+        scrolling = false;
+      });
+    }
+    scrolling = true;
+  };
+
   onMount(() => {
-    const virtuallTable = new VirtualList({
+    virtuallTable = new VirtualList({
+      domRef: virtuallTableDom,
       rowHeight: 42,
       height: 600,
       dataList: Array(600)
         .fill(0)
         .map((item, index) => ({ id: `${index}` })),
     });
-    virtuallTable.mount();
-    document.getElementById("table").appendChild(virtuallTable.domRef);
+    virtuallTable.domRef.addEventListener("scroll", handleScroll);
+    scrollHeight =
+      virtuallTable.domRef.scrollHeight - virtuallTable.domRef.clientHeight;
   });
+  $: {
+    if (virtuallTableDom) {
+      virtuallTableDom.scrollTop = scrollTop;
+    }
+  }
 </script>
 
-<div id="table" />
+scrollTop: {scrollTop}
+scrollLeft: {scrollLeft}
+<div bind:this={virtuallTableDom} />
+<input
+  type="range"
+  bind:value={scrollTop}
+  max={scrollHeight}
+  style="width: 1000px;"
+/>
--- src/lib/VirtualList.ts
+++ src/lib/VirtualList.ts
@@ -9,6 +9,8 @@ class VirtualList {
   fieldList: VirtualField[];
   getRowUniqKey: Function;
   rowHeight: number;
+  scrollTop: number = 0;
+  scrollLeft: number = 0;
   rowList: { id: string | Symbol, data: { id: string | Symbol }, rowdom: HTMLDivElement, height?: number }[];
 
 
@@ -19,6 +21,7 @@ class VirtualList {
     if (height) this.domRef.style.height = height + 'px'
     if (tableClass) this.domRef.className += ' ' + tableClass
     this.domRef.setAttribute("id", tableId || 'VirtualTableId__' + uniqueId());
+    this.domRef.style.overflow = 'auto'
     this.dataList = dataList;
     this.fieldList = fieldList
     this.dataMap = dataMap
@@ -32,19 +35,35 @@ class VirtualList {
       rowdom.setAttribute("id", id);
       rowdom.style.height = height + 'px'
       rowdom.style.width = '100%'
-      rowdom.style.backgroundColor = 'red'
+      rowdom.style.backgroundColor = 'pink'
+
       const rowTop = top;
       top += height;
+      rowdom.innerHTML = 'Row' + id + '     rowTop:' + rowTop
       return { id, data, rowdom, height, top: rowTop }
     })
     this.rowList = rowList
+    this.mount()
   }
 
   mount() {
+    console.log('99999');
     this.domRef.append(...this.rowList.map(({ rowdom }) => rowdom))
   }
 
+  update() {
+
+  }
+
+  scroll(scrollTop: number) {
+    this.scrollTop = scrollTop
+
+  }
+
   get rows() {
+    // this.rowList.forEach(({ top }) => {
+
+    // })
     return this.dataList;
   }
   set rows(options) { }
